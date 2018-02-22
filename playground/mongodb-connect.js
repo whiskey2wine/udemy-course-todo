625e45435168c1893b8db7265a6c1559dc5a0bd9
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-22 12:13:47 +0700

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server.');
+    console.log(err);
+    return;
+  }
+  console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false,
+  // }, (error, result) => {
+  //   if (error) {
+  //     console.log('Unable to insert todo', error);
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Bacon',
+  //   age: 22,
+  //   location: 'Thailand',
+  // }, (error, result) => {
+  //   if (error) {
+  //     console.log('Unable to insert user', error);
+  //   }
+
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
+
+  client.close();
+});
+
---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server.');
+    console.log(err);
+    return;
+  }
+  console.log('Connected to MongoDB server.');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a8e3dd4420c7a29a8273ae0'),
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (error) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  db.collection('Users').find({ name: 'Bacon' }).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (error) => {
+    console.log('Unable to fetch todos', error);
+  });
+
+  // client.close();
+});
+
