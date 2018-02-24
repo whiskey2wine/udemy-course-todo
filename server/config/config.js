83c8867056bd44d072e9aba344f49f53f7675faa
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-25 02:50:48 +0700

    Add POST /users route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.PORT = 3000;
--- server/server.js
+++ server/server.js
@@ -106,6 +106,21 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+
+  user
+    .save()
+    .then(() => user.generateAuthToken())
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}!`);
 });
