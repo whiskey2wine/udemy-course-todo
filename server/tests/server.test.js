cfb37b3bde4d76408f58e2f67f89eb5576955546
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-24 18:29:32 +0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,43 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELECT /todos/:id', () => {
+  it('should remove todo', (done) => {
+    const hexID = todosInit[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexID);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexID)
+          .then((todo) => {
+            expect(todo).toBeFalsy();
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    const hexID = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/1234')
+      .expect(404)
+      .end(done);
+  });
+});
