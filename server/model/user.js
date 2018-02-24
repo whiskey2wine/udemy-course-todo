9c6b8cf05545290da2324e35adc11d5d07f89a3e
whiskey2wine <jayakorn.karikan@gmail.com>
2018-02-25 03:30:16 +0700

    Add GET /users/me and authentication middleware

--- server/model/user.js
+++ server/model/user.js
@@ -58,6 +58,26 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth',
+  });
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
